4f1e6ccf250519b1ba6cc7966890cc9fc387ac83
0sayed0 <115923309+0sayed0@users.noreply.github.com>
2024-02-11 15:09:22 +0530

    Update ProductController.java

--- src/main/java/com/javatechie/crud/example/controller/ProductController.java
+++ src/main/java/com/javatechie/crud/example/controller/ProductController.java
@@ -47,4 +47,7 @@ public class ProductController {
     public String deleteProduct(@PathVariable int id) {
         return service.deleteProduct(id);
     }
+
+    @GetMapping("testAPI")
+    public String testMethod() { return "i am doing well";}
 }
